a3fe84d6eb70ecb67724c25d652d9f24e51d2f42
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-05 13:07:10 +0000

    page styles add

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 'use strict'
 
-const testpage = true;
-console.log(testpage);
+// Selecting elements for the game from the html
+const score1Element = document.querySelector("#score--0");
+const score2Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+score1Element.textContent = 0;
+score2Element.textContent = 0;
+// diceElement.classList.add("hidden");
